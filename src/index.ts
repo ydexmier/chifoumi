424c41eb5cdc5267dc0ecbea11643aebc4be619b
Yoann Dexmier <yoann110690@gmail.com>
2022-02-06 01:03:20 +0100

    improve css

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 // here you put all the js you want.
+
+['paper','scissors', 'rock'].forEach(element => {
+    document.getElementById(element).addEventListener('click', () => {
+        document.getElementById(element).classList.add('selected');
+        document.getElementById('stepContainer').classList.add('end-step1');
+    });
+});
