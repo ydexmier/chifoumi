d5ce044344dcf9bc056391fc906abf5662c39698
Yoann Dexmier <yoann110690@gmail.com>
2022-02-13 20:19:33 +0100

    step-2

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,10 @@
     document.getElementById(element).addEventListener('click', () => {
         document.getElementById(element).classList.add('selected');
         document.getElementById('stepContainer').classList.add('end-step1');
+        setTimeout(() => {
+            document.getElementById('stepContainer').classList.remove('end-step1');
+            document.getElementById('stepContainer').classList.remove('step-1');
+            document.getElementById('stepContainer').classList.add('step-2');
+        } , 300)
     });
 });
