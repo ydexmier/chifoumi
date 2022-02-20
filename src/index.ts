ed151e486310ff15bbd9eb309e0277086e545b0e
Yoann Dexmier <yoann110690@gmail.com>
2022-02-21 00:16:41 +0100

    rules and mobile css

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 // here you put all the js you want.
 
+let select = null;
+let botSelect = null;
+let score = 0;
+
 const getRandomInt = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -18,9 +22,9 @@ const getResult = (botChoice, userChoice) => {
         return { increment: 0, resultClass: '' };
     }
     
-    const botIndex = [...actions, actions[0]].indexOf(botChoice);
-    const userIndex = [...actions, actions[0]].indexOf(userChoice);
-    return (userIndex + 1) === botIndex ? { increment: -1, resultClass: 'lose' } : { increment: 1, resultClass: 'win' };
+    const botIndex = actions.indexOf(botChoice);
+    const userIndex = actions.indexOf(userChoice);
+    return (userIndex + 1) % actions.length === botIndex ? { increment: -1, resultClass: 'lose' } : { increment: 1, resultClass: 'win' };
 }
 
 
@@ -30,8 +34,8 @@ const step3 = (botChoice, userChoice) => {
     container.classList.remove(...container.classList);
     container.classList.add('step-3');
     resultClass && container.classList.add(resultClass);
-    
-    document.getElementById('score').innerText = parseInt(document.getElementById('score').innerText)+increment+"";
+    score = score + increment;
+    document.getElementById('score').innerText = score + "";
 }
 
 
@@ -40,10 +44,11 @@ const loop = (actionIndex, count, nbLoop, userChoice) => {
     setTimeout(() => {
         bot.classList.remove(...actions, 'ghost');
         bot.classList.add(actions[actionIndex]);
+        botSelect = actions[actionIndex];
         if (count < nbLoop) {
             loop((actionIndex+1)%3 , count+1, nbLoop, userChoice);
         } else {
-            setTimeout(() => step3(actions[actionIndex], userChoice), 500);
+            setTimeout(() => step3(botSelect, userChoice), 500);
         }
     }, 100)
 }
@@ -76,12 +81,13 @@ export const step1 = () => {
         const element = document.getElementById(id);
         element.classList.remove('selected');
         element.addEventListener('click', function handler() {
+            select = id;
             element.classList.add('selected');
             container.classList.add('end-step1');
             removeActionsClick(handler);
             setTimeout(() => {
                 
-                step2(id);
+                step2(select);
             } , 300)
         });
     });
